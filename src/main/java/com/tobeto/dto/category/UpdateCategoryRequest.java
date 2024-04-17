e41fe03bbe9b3c2dea889aedce1b1a05c6473d20
omerozgur23 <omerozgur23@gmail.com>
2024-04-17 15:10:24 +0300

    update

--- src/main/java/com/tobeto/dto/category/UpdateCategoryRequest.java
+++ src/main/java/com/tobeto/dto/category/UpdateCategoryRequest.java
@@ -2,13 +2,9 @@ package com.tobeto.dto.category;
 
 import java.util.UUID;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class UpdateCategoryRequest {
 
 	private UUID id;
